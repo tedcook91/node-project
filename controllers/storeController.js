6cbf4ec8ca82c86f040f2417cc5ba36993e1e329
captainangus <edward.w.cook@gmail.com>
2018-07-12 23:52:12 -0400

    querying stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
+    console.log(req.name);
     res.render('index');
 };
 
@@ -12,7 +13,15 @@ exports.addStore = (req, res) => {
 
 
 exports.createStore = async (req, res) => {
-    const store = new Store(req.body);
+    const store = await (new Store(req.body)).save();
     await store.save();
-    res.redirect('/');
+    req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
+    res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async (req, res) => {
+
+    const stores = await Store.find();
+    console.log(stores);
+    res.render('stores', { title: 'Stores', stores: stores })
+}
