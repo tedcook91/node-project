481aaf8b30c8f58f6676018c7c556e1d03e263fc
captainangus <edward.w.cook@gmail.com>
2018-10-16 23:35:19 -0400

    add lat long

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,27 +1,47 @@
-const mongoose = require('mongoose');
-const Store = mongoose.model('Store');
+const mongoose = require("mongoose");
+const Store = mongoose.model("Store");
 
 exports.homePage = (req, res) => {
-    console.log(req.name);
-    res.render('index');
+  console.log(req.name);
+  res.render("index");
 };
-
 
 exports.addStore = (req, res) => {
-    res.render('editStore', { title: 'Add Store' });
+  res.render("editStore", { title: "Add Store" });
 };
 
-
 exports.createStore = async (req, res) => {
-    const store = await (new Store(req.body)).save();
-    await store.save();
-    req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
-    res.redirect(`/store/${store.slug}`);
+  const store = await new Store(req.body).save();
+  await store.save();
+  req.flash(
+    "success",
+    `Successfully Created ${store.name}. Care to leave a review?`
+  );
+  res.redirect(`/store/${store.slug}`);
 };
 
 exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  console.log(stores);
+  res.render("stores", { title: "Stores", stores: stores });
+};
 
-    const stores = await Store.find();
-    console.log(stores);
-    res.render('stores', { title: 'Stores', stores: stores })
-}
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({ _id: req.params.id });
+
+  res.render("editStore", { title: `Edit ${store.name}`, store: store });
+};
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return the new store instead of the old one
+    runValidators: true
+  }).exec();
+  req.flash(
+    "success",
+    `Successfully updated <strong> ${store.name}</strong>. <a href="/stores/${
+      store.slug
+    }">View Store</a>`
+  );
+  res.redirect(`/stores/${store._id}/edit`);
+};
