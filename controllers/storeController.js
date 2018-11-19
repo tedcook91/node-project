4630d1522f7ece51f2d5230fb699078937a96d86
captainangus <edward.w.cook@gmail.com>
2018-11-18 21:24:39 -0500

    add showpage

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -81,3 +81,9 @@ exports.updateStore = async (req, res) => {
   );
   res.redirect(`/stores/${store._id}/edit`);
 };
+
+exports.getStoreBySlug = async (req, res, next) => {
+  const store = await Store.findOne({ slug: req.params.slug })
+  if(!store) return next();
+  res.render('store', { store, title: store.name });
+}
