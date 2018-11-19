b0bea55065350af1517dab909167e42e024d2d15
captainangus <edward.w.cook@gmail.com>
2018-11-18 21:49:05 -0500

    fixes

--- models/Store.js
+++ models/Store.js
@@ -35,12 +35,18 @@ const storeSchema = new mongoose.Schema({
     photo: String
 });
 
-storeSchema.pre('save', function(next) {
+storeSchema.pre('save', async function(next) {
     if (!this.isModified('name')) {
         next();
         return;
     }
     this.slug = slug(this.name);
+    //find other stores that have a slug of ted, ted-1, ted-2
+    const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+    const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
+    if(storesWithSlug.length) {
+        this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+    }
     next();
 });
 
